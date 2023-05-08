70fa2f92f9892096a055e6c6d2e02b967f30359c
LeaJaeyoung <pt0108@naver.com>
2023-05-08 20:55:15 +0900

    update

--- my-app/src/pages/Contact.js
+++ my-app/src/pages/Contact.js
@@ -3,12 +3,22 @@ import '../styles/Contact.css';
 
 function Contact() {
     return (
-        <Container id="Contact" style={{ minHeight: "100vh"}}>
+        <Container id="Contact" style={{ minHeight: "80vh"}}>
             <h2 className="page-title">CONTACT</h2>
-            <Container style={{ border: "1px solid red", marginTop:"60px"}}>
+            <Container id="contact-contain" className="row justify-content-between">
                 <Container className="contact-box">
-                    <img src="img/calling.gif" alt="calling gif" />
+                    <img src="img/calling.gif" alt="phone number" />
                     <h5>010-7414-3210</h5>
+                </Container>
+                <Container className="contact-box">
+                    <img src="img/letter.gif" alt="email" />
+                    <h5>pt0108@naver.com</h5>
+                </Container>
+                <Container className="contact-box">
+                    <a href="https://github.com/pt0108" target="_blank" rel="noopener noreferrer">
+                        <img src="img/browser.gif" alt="github link" />
+                        <h5>https://github.com/pt0108</h5>
+                    </a>
                 </Container>
             </Container>
         </Container>
