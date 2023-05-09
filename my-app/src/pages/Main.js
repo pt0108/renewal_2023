ab0f431dc71798760764afda568803354a3e44af
LeaJaeyoung <pt0108@naver.com>
2023-05-09 17:51:00 +0900

    update

--- my-app/src/pages/Main.js
+++ my-app/src/pages/Main.js
@@ -1,9 +1,13 @@
 import { Container } from "react-bootstrap";
+import '../styles/Main.css';
 
 function Main() {
     return (
         <Container id="Main" style={{ minHeight: "100vh"}}>
-            Main
+            <div className="main-bg">
+                <span>안녕하세요,</span>
+                <span>웹 퍼블리셔를 꿈꾸는 <br/> 이재영입니다.</span>
+            </div>
         </Container>
     );
 }
