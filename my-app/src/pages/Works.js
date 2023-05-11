4fc56650989f083ea92a09b01a2bcbc20b60b738
LeaJaeyoung <pt0108@naver.com>
2023-05-11 19:04:25 +0900

    update

--- my-app/src/pages/Works.js
+++ my-app/src/pages/Works.js
@@ -1,9 +1,17 @@
 import { Container } from "react-bootstrap";
+import '../styles/Works.css';
 
 function Works() {
     return (
         <Container id="Works" style={{ minHeight: "100vh"}}>
             <h2 className="page-title">WORKS</h2>
+            <Container id="works-contents">
+                <Container className="works-content"></Container>
+                <Container className="works-content"></Container>
+                <Container className="works-content"></Container>
+                <Container className="works-content"></Container>
+            </Container>
+            <div className="works-bg"></div>
         </Container>
     );
 }
